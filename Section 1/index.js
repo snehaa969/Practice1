5c172aa9dd558098ceb05c259bcad89dc556f7ea
snehaa969 <snehasingh2001march@gmail.com>
2023-03-19 15:15:28 +0530

    init

---
+++ Section 1/index.js
@@ -0,0 +1,25 @@
+console.log('Hello World');
+
+// let a = "Sneha"
+// let b = 6
+// console.log(a+b)
+
+// console.log(typeof (a+b))
+
+// //const a = {
+//     name: "Harry",
+//     section: 1,
+//     isprincipal: false
+// } 
+// //a1 = 45
+
+// a1['friend']="Reddy"
+// console.log(a1)
+
+console.log("Operators in JS")
+let a = 45;
+let b = 4;
+console.log("a+b = ", a+b);
+console.log("a-b = ", a-b);
+console.log("a/b = ", a/b);
+console.log("a**b = ",a**b);
